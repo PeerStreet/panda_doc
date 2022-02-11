65307b4f968003b4bd34e27dddfe045699086760
Amador Custer <custeramador@gmail.com>
2022-02-10 21:50:58 -0600

    mark :has_completed as optional

--- lib/panda_doc/objects/recipient.rb
+++ lib/panda_doc/objects/recipient.rb
@@ -8,7 +8,7 @@ module PandaDoc
       attribute :first_name, Types::Coercible::String.optional
       attribute :last_name, Types::Coercible::String.optional
       attribute :recipient_type, Types::Coercible::String
-      attribute :has_completed, Types::Params::Bool
+      attribute? :has_completed, Types::Params::Bool
       attribute? :role, Types::Coercible::String.optional
       attribute? :type, Types::Coercible::String.optional
     end
